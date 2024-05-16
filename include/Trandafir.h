2dc69fa21b038914aecdba577a1a447ad239c200
IoanaBejenaru3 <ioana.75@yahoo.com>
2024-05-16 22:37:57 +0300

    New commit4

--- include/Trandafir.h
+++ include/Trandafir.h
@@ -7,16 +7,17 @@ private:
     int numar_spini;
 public:
     //CONSTRUCTORI
-    Trandafir(std::string denumire_ = "", const int numar_boboci_ = 0, const int durata_viata_ = 0, std::string culoare_ = "", const int numar_spini_ = 0);
-
+    Trandafir(std::string denumire_ = "", const int numar_boboci_ = 0, const int durata_viata_ = 0, const int zi_plantare_ = 0, std::string culoare_ = "", const int numar_spini_ = 0);
+    //constructor de copiere
+    Trandafir(const Trandafir& other);
 
     //OPERATORI
-    
+    Trandafir& operator=(const Trandafir& other);
 
     //FUNCTII
     void Descrie(std::ostream& os) const override;
     //functie specifica acestei derivate, deci va aparea dynamic_cast
-    void Atentioneaza();
+    void Atentioneaza() const;
 
     //DESTRUCTOR
     ~Trandafir();
---
+++ src/Trandafir.cpp
@@ -0,0 +1,34 @@
+#include "../include/Trandafir.h"
+
+//CONSTRUCTORI
+
+Trandafir::Trandafir(std::string denumire_, const int numar_boboci_, const int durata_viata_, const int zi_plantare_, std::string culoare_, const int numar_spini_) : Floare(denumire_, numar_boboci_, durata_viata_, zi_plantare_, culoare_), numar_spini{numar_spini_} {}
+
+//constructor de copiere
+Trandafir::Trandafir(const Trandafir& other) : Floare(other), numar_spini{other.numar_spini} {}
+
+
+//OPERATORI
+
+//operator de atribuire prin copiere
+Trandafir& Trandafir::operator=(const Trandafir& other)
+{
+    //obiectului curent ii aplicam static cast pentru a putea folosi operatorul egal suprascris acolo
+    static_cast <Floare&> (*this) = other;
+    //acum ne ocupam de partea specifica ob de tip Trandafir
+    numar_spini = other.numar_spini;
+    return *this;
+}
+
+
+//FUNCTII
+
+void Trandafir::Descrie(std::ostream& os) const
+{
+    os << "Trandafirul este o floare elegantă și parfumată, cunoscută pentru petalele sale delicate și colorate. Este un simbol al iubirii și frumuseții.\n";
+}
+
+void Trandafir::Atentioneaza() const
+{
+    std::cout << "ATENTIE! Nu te intepa in spini! \n";
+}
